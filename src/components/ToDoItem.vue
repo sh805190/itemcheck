baed9c3741a89c347de91c44d892fed1e1a7dbab
Shuo_Yang <sh805190@dal.ca>
2021-05-19 21:26:51 -0300

    haha

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <input type="checkbox" id="todo-item" />
+        <label for="todo-item">{{label}}</label>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        label:{required:true, type:String}
+        ,done:{default:false, type:Boolean}
+    }
+};
+</script>
